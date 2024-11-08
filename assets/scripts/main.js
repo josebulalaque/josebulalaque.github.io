c7e3be54a50e8381481da6f29b8f56136fd7a061
user <user@debx250.local>
2024-11-09 04:30:59 +0800

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,14 @@
+const body = document.body
+
+const div = document.querySelector("div")
+const spanHi = document.querySelector("#hi")
+const spanBye = document.querySelector("#bye")
+
+
+spanHi.classList.add("new-class")
+spanHi.classList.remove("hi1")
+spanHi.classList.toggle("hi2")
+
+
+
+
